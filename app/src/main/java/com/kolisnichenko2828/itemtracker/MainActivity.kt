ccb7ec1559d3b65bc4ac2ef296dbca1eab455655
kolisnichenko2828 <kolisnichenko2828@gmail.com>
2026-03-02 10:51:30 +0200

    initial commit

---
+++ app/src/main/java/com/kolisnichenko2828/itemtracker/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.kolisnichenko2828.itemtracker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.kolisnichenko2828.itemtracker.theme.ItemTrackerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ItemTrackerTheme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    Box(
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+
+                    }
+                }
+            }
+        }
+    }
+}
